7f938de1d8ec4c0c91504771b58314bde22b9da5
Amir Hamza <hamza_amir@yahoo.com>
2022-11-07 17:50:36 +0600

    Jagged Array and Multidimensional Array

---
+++ P0001_Jagged_Array/Program.cs
@@ -0,0 +1,166 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Example 1
+        int[][] arr1 = new int[5][];
+
+        for (int i = 0; i < arr1.Length; i++)
+        { 
+            arr1[i] = new int[3];
+        }
+
+        // this code will print 0
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            for (int j = 0; j < arr1[i].Length; j++)
+            {
+                Console.Write(arr1[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // change values with -1
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            for (int j = 0; j < arr1[i].Length; j++)
+            {              
+                arr1[i][j] = -1;          
+            }           
+        }
+
+        
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            for (int j = 0; j < arr1[i].Length; j++)
+            {
+               Console.Write(arr1[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        // Example 2. print 1 2 3 4 ... n
+
+        // initialize with 1 2 3 4 ... n
+        int k = 11;
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            for (int j = 0; j < arr1[i].Length; j++)
+            {
+
+                arr1[i][j] = k;
+                k++;
+            }
+            
+        }
+
+        // print 1 2 3 4 ... n
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            for (int j = 0; j < arr1[i].Length; j++)
+            {
+                Console.Write(arr1[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+
+
+        //Example 3. print with index
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            for (int j = 0; j < arr1[i].Length; j++)
+            {
+                Console.Write("[{0}][{1}] = {2}  ", i, j, arr1[i][j]);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        // Example 4. another array. 
+
+        int[][] arr2 = new int[3][];
+        arr2[0] = new int[] { 10, 20, 30};
+        arr2[1] = new int[] { 40, 50 };
+        arr2[2] = new int[] { 40, 50, 60 };
+
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            for (int j = 0; j < arr2[i].Length; j++)
+            {
+                Console.Write(arr2[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        //Example 5. another array
+
+        int[][] arr3 = new int[3][] {
+            new int[] { 10, 20},
+            new int[] {30, 40, 50 },
+            new int [] {100 }
+        };
+
+        for (int i = 0; i < arr3.Length; i++)
+        {
+            for (int j = 0; j < arr3[i].Length; j++)
+            {
+                Console.Write(arr3[i][j] + " ");
+                //Console.WriteLine("arr3[{0}][{1}] = {2} ", i, j, arr3[i][j]);
+            }
+            Console.WriteLine();
+        }
+
+        // Transpose
+
+        int[][] Transpose(int[][] matrix)
+        {
+            var trans = GenerateArray(matrix[0].Length, matrix.Length, 0);
+
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                for (var j = 0; j < matrix[0].Length; j++)
+                {
+                    trans[j][i] = matrix[i][j];
+                }
+            }
+            return trans;
+        }
+        static T[][] GenerateArray<T>(int row, int Col, T value)
+        {
+            var arr = new T[row][];
+
+            for (int i = 0; i < row; i++)
+            {
+                arr[i] = new T[Col];
+                for (int j = 0; j < Col; j++)
+                {
+                    arr[i][j] = value;
+                }
+            }
+            return arr;
+        }
+
+
+        // use of the above transpose method 
+        int[][] trans_of_arr1 = Transpose(arr1);
+
+        for (int i = 0; i < trans_of_arr1.Length; i++)
+        {
+            for (int j = 0; j < trans_of_arr1[i].Length; j++)
+            {
+                Console.Write(trans_of_arr1[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+}
---
+++ P0002_Multidimensional_Array/Program.cs
@@ -0,0 +1,134 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] arr = new int[5, 5];
+        int k = 11;
+        int a = arr.GetLength(0);
+        int b = arr.GetLength(1);
+
+        Console.WriteLine("a = " + a + ", b =" + b);
+        Console.WriteLine();
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = k;
+                k++;
+            }
+        }
+
+        // for printing horizontally
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        // for printing vertically. // There is a problem. Row and column number changed.
+        for (int i = 0; i < arr.GetLength(1); i++)
+        {
+            for (int j = 0; j < arr.GetLength(0); j++)
+            {
+
+                Console.Write(arr[j, i] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+
+        // for printing vertically
+        // if rows and columns are not equal, this code will occur runtime exception. so I have to implement another trick.
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+
+                Console.Write(arr[j, i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+
+        //Example Question and answer
+
+        //Question. write 11 to 25 Horizontally in 5 rows and 3 columns
+        /*
+        Print
+        11  12  13
+        14  15  16
+        17  18  19
+        20  21  22
+        23  24  25
+        */
+
+        int[,] horizontallyPrint = new int[5, 3];
+        int hpValue = 11;
+
+        for (int i = 0; i < horizontallyPrint.GetLength(0); i++)
+        {
+            for (int j = 0; j < horizontallyPrint.GetLength(1); j++)
+            {
+                horizontallyPrint[i, j] = hpValue;
+                hpValue++;
+            }
+        }
+        for (int i = 0; i < horizontallyPrint.GetLength(0); i++)
+        {
+            for (int j = 0; j < horizontallyPrint.GetLength(1); j++)
+            {
+                Console.Write(horizontallyPrint[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+
+
+
+        //Question. write 11 to 25 vertically in 5 rows and 3 columns
+        /*
+        Print
+        11  16  21
+        12  17  22
+        13  18  23
+        14  19  24
+        15  20  25
+        */
+
+        // My answer
+        // This is the trick to print vertically without changing rows and column numbers (in output).
+
+        int[,] verticallyPrint = new int[3, 5];
+        int vpValue = 11;
+
+        for (int i = 0; i < verticallyPrint.GetLength(0); i++)
+        {
+            for (int j = 0; j < verticallyPrint.GetLength(1); j++)
+            {
+                verticallyPrint[i, j] = vpValue;
+                vpValue++;
+            }
+        }
+        // transpose
+        for (int i = 0; i < verticallyPrint.GetLength(1); i++)
+        {
+            for (int j = 0; j < verticallyPrint.GetLength(0); j++)
+            {
+                Console.Write(verticallyPrint[j, i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
